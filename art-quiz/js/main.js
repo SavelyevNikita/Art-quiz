fcfe2163046edcb5dab8cfafe92c9034a44a0dc9
NikitaSavelyev <nik2111122@sibmail.com>
2021-11-17 00:42:39 +0300

    feat:add json images

---
+++ art-quiz/js/main.js
@@ -0,0 +1,5 @@
+import images from "./images.js";
+// images.map(item => console.log(item));
+const frameForMainImage = document.querySelector('main');
+frameForMainImage.style.backgroundImage = none; //('../img/img/0.webp');
+console.log(images[0]);
